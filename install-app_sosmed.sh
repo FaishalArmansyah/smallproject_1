3a6187a9e210f9897b4537d5a1ee105bd7b70672
faishalarmansyah <faishal@linuxmail.org>
2022-05-25 16:58:53 +0700

    update script

--- install-app_sosmed.sh
+++ install-app_sosmed.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-echo "===== Installasi LAMP & Wordpress ====="
+echo "===== Installasi Apache & PHP ====="
 echo "===== Step 1 - Install Apache ====="
 sudo apt update
 sudo apt install -y apache2 apache2-utils
 sudo systemctl start apache2 && sudo systemctl enable apache2
-echo "===== Step 2 - Install MariaDB ====="
-sudo apt install mariadb-server mariadb-client -y
-sudo systemctl start mariadb && sudo systemctl enable mariadb
-echo "===== Step 3 - Install PHP 7.4 ====="
+echo "===== Step 2 - Install PHP 7.4 ====="
 sudo apt install php7.4 libapache2-mod-php7.4 php7.4-mysql php-common php7.4-cli php7.4-common php7.4-json php7.4-opcache php7.4-readline -y
 sudo a2enmod php7.4
 sudo systemctl restart apache2
-echo "===== Step 4 - Download Wordpress ====="
+echo "===== Step 3 - Download App ====="
 #git clone https://github.com/FaishalArmansyah/smallproject_1.git
 sudo cp -r smallproject_1/sosial-media /var/www/
 sudo chown www-data:www-data /var/www/sosial-media/ -R
